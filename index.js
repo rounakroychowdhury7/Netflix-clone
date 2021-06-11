ed110110b20ae629887440420fe0cf04ec490bef
rounakroychowdhury7 <84000739+rounakroychowdhury7@users.noreply.github.com>
2021-06-11 21:27:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function myClick(){
+    document.getElementById("links-data").classList.toggle("active");
+}
+function myClick1(){
+    document.getElementById("links-data1").classList.toggle("active1");
+}
+function myClick2(){
+    document.getElementById("links-data2").classList.toggle("active2");
+}
+function myClick3(){
+    document.getElementById("links-data3").classList.toggle("active3");
+}
+function myClick4(){
+    document.getElementById("links-data4").classList.toggle("active4");
+}
+function myClick5(){
+    document.getElementById("links-data5").classList.toggle("active5");
+}
